27e20c47cd16c19eb79f95826d8b5a373e683dd2
BeatrizG <beatrizgarciaang0@gmail.com>
2022-12-24 15:40:48 -0300

    Se agrega modificaciones de cerrar menu responsive

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
 document.getElementById("back_menu").addEventListener("click", ocultar_menu);
+document.getElementById("menu-close").addEventListener("click", ocultar_menu);
 
 nav = document.getElementById("nav");
 background_menu = document.getElementById("back_menu");
